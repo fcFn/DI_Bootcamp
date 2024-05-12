4f9d8f19221fd860dde4f84a6e00d4d02358ed79
fcFn <fcfn@users.noreply.github.com>
2024-05-12 20:29:55 +0300

    Add daily challenge for W3D2

---
+++ week3/day2/daily-challenge/exercise1.js
@@ -0,0 +1,9 @@
+const fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+fruits.shift();
+fruits.sort();
+fruits.push("Kiwi");
+fruits.splice(0, 1);
+fruits.reverse();
+
+console.log(fruits);
---
+++ week3/day2/daily-challenge/exercise2.js
@@ -0,0 +1,3 @@
+const moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+
+console.log(moreFruits[1][1][0]); 
