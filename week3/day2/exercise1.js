18adc5f4a16090582906277451bc02014d6b960d
fcFn <fcfn@users.noreply.github.com>
2024-05-12 20:17:22 +0300

    Add W3D2 exercises

---
+++ week3/day2/exercise1.js
@@ -0,0 +1,4 @@
+const food = "pasta";
+const meal = "dinner";
+
+console.log(`I love eating ${food} at every ${meal}.`);
---
+++ week3/day2/exercise3.js
@@ -0,0 +1,9 @@
+function celsiusToFahrenheit(celsius) {
+  return (celsius * 9) / 5 + 32;
+}
+
+const celsius = 42;
+
+console.log(`${celsius}°C is ${celsiusToFahrenheit(celsius)}°F`);
+
+
