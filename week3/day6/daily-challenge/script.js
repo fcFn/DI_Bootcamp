5a8e1760844ef0e31a18e2bf72d2df1960733682
fcFn <fcfn@users.noreply.github.com>
2024-05-25 20:32:26 +0300

    Format W3D6 DC script

--- week3/day6/daily-challenge/script.js
+++ week3/day6/daily-challenge/script.js
@@ -2,23 +2,23 @@
 // Write a JavaScript program that recreates the pattern below.
 // Do this challenge twice: first by using one loop, then by using two nested for loops (Nested means one inside the other - check out this tutorial of nested loops).
 // Do this Daily Challenge by yourself, without looking at the answers on the internet.
-// *  
-// * *  
-// * * *  
-// * * * *  
+// *
+// * *
+// * * *
+// * * * *
 // * * * * *
 // * * * * * *
 
-let str = ""
+let str = "";
 for (i = 0; i < 6; i++) {
-  str += '* '
-  console.log(str)
+  str += "* ";
+  console.log(str);
 }
 
 for (i = 0; i < 6; i++) {
-  let str = ""
+  let str = "";
   for (j = -1; j < i; j++) {
-    str += '* '
+    str += "* ";
   }
-  console.log(str)
+  console.log(str);
 }
